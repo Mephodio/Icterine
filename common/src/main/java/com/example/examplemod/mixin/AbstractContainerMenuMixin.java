e5a736e72de013d9ff9e1487ef610c10e2934fc1
Mephody <mephodio@gmail.com>
2023-12-01 23:44:08 +0300

    triggerSlotListeners injection test

--- common/src/main/java/com/example/examplemod/mixin/AbstractContainerMenuMixin.java
+++ common/src/main/java/com/example/examplemod/mixin/AbstractContainerMenuMixin.java
@@ -2,6 +2,7 @@ package com.example.examplemod.mixin;
 
 import com.google.common.base.Suppliers;
 import net.minecraft.core.NonNullList;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
@@ -11,8 +12,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -27,6 +32,9 @@ abstract class AbstractContainerMenuMixin {
     @Final
     @Shadow
     private List<DataSlot> dataSlots;
+
+    AbstractContainerMenuMixin() {
+    }
 
     @Shadow
     protected abstract void triggerSlotListeners(int i, ItemStack itemStack, Supplier<ItemStack> supplier);
@@ -43,10 +51,44 @@ abstract class AbstractContainerMenuMixin {
     @Shadow
     protected abstract void synchronizeDataSlotToRemote(int i, int j);
 
+    @Unique
+    private ItemStack prevItemStack;
+
     @Redirect(method = "addSlot", at = @At(value="INVOKE", target = "Lnet/minecraft/core/NonNullList;add(Ljava/lang/Object;)Z", ordinal = 1))
-    protected boolean addSlot(NonNullList lastSlots, Object emptyStack, Slot slot) {
-        lastSlots.add(slot.getItem());
+    protected boolean addSlot(NonNullList<ItemStack> lastSlots, Object emptyStack, Slot slot) {
+        if (slot.container instanceof Inventory) {
+            /*
+                 YOU CANT DO THIS, you cannot check class of containerMenu, player slots are in all inventories.
+                 Check slot container, if available
+
+                 And in `triggerSlotListeners` you somehow need to detect is it ServerPlayer or not,
+                 and same thing with RecipeBookMenu check
+             */
+            lastSlots.add(slot.getItem());
+        } else {
+            lastSlots.add(ItemStack.EMPTY);
+        }
         return true;
+    }
+
+    @Inject(method = "triggerSlotListeners",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;set(ILjava/lang/Object;)Ljava/lang/Object;"),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private void triggerSlotListenersSaveOld(int slotNumber, ItemStack newStack,
+            Supplier<ItemStack> newStackSupplier, CallbackInfo ci, ItemStack oldStack) {
+//        System.out.println("stack changed: " + oldStack.toString() + " " + newStack.toString());
+        prevItemStack = oldStack;
+    }
+
+    @Redirect(method = "triggerSlotListeners", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ContainerListener;slotChanged(Lnet/minecraft/world/inventory/AbstractContainerMenu;ILnet/minecraft/world/item/ItemStack;)V"))
+    private void triggerSlotListeners(ContainerListener listener, AbstractContainerMenu abstractContainerMenu, int i, ItemStack itemStack) {
+        if (abstractContainerMenu instanceof RecipeBookMenu
+                && prevItemStack.getItem().equals(itemStack.getItem())
+                && prevItemStack.getCount() > itemStack.getCount()) {
+            System.out.println("same item decreased " + prevItemStack.toString());
+            return;
+        }
+        listener.slotChanged(abstractContainerMenu, i, itemStack);
     }
 
 //    @Redirect(method = "addSlotListener", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/AbstractContainerMenu;broadcastChanges()V"))
