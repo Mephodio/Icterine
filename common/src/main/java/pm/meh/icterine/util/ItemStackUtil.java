2279e6be04c18a318cb731a0533652a0261a0b81
Mephody <mephodio@gmail.com>
2023-12-03 16:11:34 +0300

    Update processItemStackInTriggerSlotListeners-related javadocs

--- common/src/main/java/pm/meh/icterine/util/ItemStackUtil.java
+++ common/src/main/java/pm/meh/icterine/util/ItemStackUtil.java
@@ -8,6 +8,17 @@ public class ItemStackUtil {
      * Compare old and new stacks and mark new stack with `lastChangeDecreasedStack`
      * if those stacks contain the same item, and the size of the new stack is smaller
      * than the old one.
+     * <br>
+     * When slot content changes, this method is used to determine if change was caused
+     * by decreasing stack size.
+     * <br>
+     * For some reason (differences in decompiling/mapping maybe?), on Forge and Fabric
+     * this mixin detects different local variables.
+     * <br> On forge: `ItemStack`, `boolean`, `ItemStack`
+     * <br> On fabric: `ItemStack`, `ItemStack`
+     * <br>
+     * So we need platform-specific mixins with different locals sets.
+     *
      * @param oldStack previous ItemStack
      * @param newStack new ItemStack
      */
